467998a6bacc9da9898138278642a8028f95cb20
Sabrina <sab.shine.art@gmail.com>
2023-01-19 12:34:58 +0100

    x and y avg movement adjusted

--- checkers/game_v2.py
+++ checkers/game_v2.py
@@ -12,7 +12,8 @@ class Game:
 
         self.board_piece_height = 23
         self.zero_posistion = [105, -95] # [106.53, -94.2, 23.53]
-        self.avg_movement = 27.0
+        self.avg_x_movement = 27.5
+        self.avg_y_movement = 27.0
         self.arm_height = 120
         
         self.swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'}, cmd_pend_size=2, callback_thread_pool_size=1)
@@ -177,7 +178,7 @@ class Game:
         row = pos[0]
         col = pos[1]
 
-        x = self.zero_posistion[0] + (row * self.avg_movement)
-        y = self.zero_posistion[1] + (col * self.avg_movement)
+        x = self.zero_posistion[0] + (row * self.avg_x_movement)
+        y = self.zero_posistion[1] + (col * self.avg_y_movement)
 
         return x, y
