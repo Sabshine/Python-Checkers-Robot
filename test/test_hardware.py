837d60d1bd224b05c63be32676372dcacfa65473
Sabrina <sab.shine.art@gmail.com>
2023-01-24 12:44:20 +0100

    added while true

--- test/test_hardware.py
+++ test/test_hardware.py
@@ -8,12 +8,13 @@ led_player = gpiozero.Button(23)
 led_computer = gpiozero.Button(24)
 
 def main():
-  if button_move.is_pressed:
-    led_player.on()
-    print("move")
+  while True:
+    if button_move.is_pressed:
+      led_player.on()
+      print("move")
 
-  if button_reset.is_pressed:
-    led_computer.on()
-    print("reset")
+    if button_reset.is_pressed:
+      led_computer.on()
+      print("reset")
 
 main()
