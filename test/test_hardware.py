b74c52ef98d612e753fef7381a81f9500beb92fc
Baeshee <basbarg@gmail.com>
2023-01-24 18:50:45 +0100

    changed test_hardware to test buttons and leds

--- test/test_hardware.py
+++ test/test_hardware.py
@@ -10,9 +10,13 @@ led_computer = gpiozero.LED(24)
 def main():
   while True:
     if button_move.is_pressed:
+      led_player.on()
+      led_computer.off()
       print("move")
 
-    # if button_reset.is_pressed:
-    #   print("reset")
+    if button_reset.is_pressed:
+      led_player.off()
+      led_computer.on()
+      print("reset")
 
 main()
