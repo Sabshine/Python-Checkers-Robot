f88fe992038fa877d5d89124520e1ccc8b9736e5
Sabrina <sab.shine.art@gmail.com>
2023-01-24 14:29:23 +0100

    cleaned test

--- test/test_hardware.py
+++ test/test_hardware.py
@@ -8,14 +8,11 @@ led_player = gpiozero.LED(23)
 led_computer = gpiozero.LED(24)
 
 def main():
-  led_player.blink()
-  led_computer.blink()
-
   while True:
-    if button_move.is_pressed:
+    if not button_move.is_pressed:
       print("move")
 
-    if button_reset.is_pressed:
-      print("reset")
+    # if button_reset.is_pressed:
+    #   print("reset")
 
 main()
